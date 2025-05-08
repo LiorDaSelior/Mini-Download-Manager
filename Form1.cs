7d8384c1de8bac81839de89977e484c1a3145a4d
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2025-05-08 14:47:22 +0300

    added json fetcher

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using Mini_Download_Manager.Service.Fetcher;
+using System.Runtime.InteropServices.JavaScript;
+
 namespace Mini_Download_Manager
 {
     public partial class Form1 : Form
@@ -5,6 +8,13 @@ namespace Mini_Download_Manager
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var jf = new JsonFetcher();
+            string res =  await jf.fetch("https://4qgz7zu7l5um367pzultcpbhmm0thhhg.lambda-url.us-west-2.on.aws/");
+            textBox1.Text = res;
         }
     }
 }
---
+++ Service/Fetcher/JsonFetcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Download_Manager.Service.Fetcher
+{
+    internal class JsonFetcher
+    {
+        private HttpClient client;
+
+        public JsonFetcher()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<String> fetch(String url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
