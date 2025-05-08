72cba0bb27109c6c9c92254aa5b33bc83689a78b
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2025-05-08 15:32:53 +0300

    added-form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,53 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            DownloadButton = new Button();
+            label1 = new Label();
+            FileNameLabel = new Label();
+            FileIconBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)FileIconBox).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // DownloadButton
             // 
-            button1.Location = new Point(359, 387);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            DownloadButton.Location = new Point(350, 365);
+            DownloadButton.Name = "DownloadButton";
+            DownloadButton.Size = new Size(75, 23);
+            DownloadButton.TabIndex = 0;
+            DownloadButton.Text = "Download";
+            DownloadButton.UseVisualStyleBackColor = true;
+            DownloadButton.Click += button1_Click;
             // 
-            // textBox1
+            // label1
             // 
-            textBox1.Location = new Point(130, 90);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(529, 23);
-            textBox1.TabIndex = 1;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold | FontStyle.Underline);
+            label1.Location = new Point(220, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(340, 37);
+            label1.TabIndex = 3;
+            label1.Text = "Mini-Download Manager";
             // 
-            // textBox2
+            // FileNameLabel
             // 
-            textBox2.Location = new Point(130, 161);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(529, 23);
-            textBox2.TabIndex = 2;
+            FileNameLabel.AutoSize = true;
+            FileNameLabel.Location = new Point(359, 122);
+            FileNameLabel.Name = "FileNameLabel";
+            FileNameLabel.Size = new Size(57, 15);
+            FileNameLabel.TabIndex = 4;
+            FileNameLabel.Text = "FileName";
+            // 
+            // FileIconBox
+            // 
+            FileIconBox.Location = new Point(329, 175);
+            FileIconBox.Name = "FileIconBox";
+            FileIconBox.Size = new Size(117, 107);
+            FileIconBox.TabIndex = 5;
+            FileIconBox.TabStop = false;
+            FileIconBox.Visible = false;
+            FileIconBox.Click += pictureBox1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
+            Controls.Add(FileIconBox);
+            Controls.Add(FileNameLabel);
+            Controls.Add(label1);
+            Controls.Add(DownloadButton);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load_1;
+            ((System.ComponentModel.ISupportInitialize)FileIconBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private Button DownloadButton;
+        private Label label1;
+        private Label FileNameLabel;
+        private PictureBox FileIconBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualBasic.Logging;
 using Mini_Download_Manager.Service.Fetcher;
 using Mini_Download_Manager.Service.FileLogic;
 using System.Collections.Generic;
@@ -7,28 +8,45 @@ namespace Mini_Download_Manager
 {
     public partial class Form1 : Form
     {
+        private JsonFetcher fetcher;
+        private FileLogic logic;
+
         public Form1()
         {
             InitializeComponent();
+            fetcher = new JsonFetcher();
+            logic = new FileLogic();
         }
 
+
+        private async void Form1_Load_1(object sender, EventArgs e)
+        {
+            DownloadButton.Enabled = false;
+            FileNameLabel.Text = "Please wait...";
+
+            var res = await fetcher.fetch("https://4qgz7zu7l5um367pzultcpbhmm0thhhg.lambda-url.us-west-2.on.aws/");
+
+            var best = this.logic.getValidHighestScoreFile(res);
+
+            FileIconBox.Load(best.ImageUrl);
+            FileNameLabel.Text = best.Title;
+            FileIconBox.Visible = true;
+
+            DownloadButton.Enabled = true;
+        }
         private async void button1_Click(object sender, EventArgs e)
         {
-            button1.Enabled = false;
 
-            var jf = new JsonFetcher();
-            var res =  await jf.fetch("https://4qgz7zu7l5um367pzultcpbhmm0thhhg.lambda-url.us-west-2.on.aws/");
-            textBox1.Text = "";
-            foreach (var item in res)
-            {
-                textBox1.Text += item.ToString();
-            }
+        }
 
-            var fl = new FileLogic();
-            var best = fl.getValidHighestScoreFile(res);
-            textBox2.Text = best.ToString();
+        private void label2_Click(object sender, EventArgs e)
+        {
 
-            button1.Enabled = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
